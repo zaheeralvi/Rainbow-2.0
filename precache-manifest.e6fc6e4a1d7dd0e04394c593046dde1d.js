80467b059bf9580ccc6af1540cf9d163a39ea543
zaheer <zaheermalik284@gmail.com>
2019-10-02 11:01:58 +0500

    Updates

---
+++ precache-manifest.e6fc6e4a1d7dd0e04394c593046dde1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2da908ee53af72d40ac87fa29d5e30",
+    "url": "/Rainbow-2.0/index.html"
+  },
+  {
+    "revision": "35cac38a3e6741a2c6f6",
+    "url": "/Rainbow-2.0/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "11a0481a58246bdcbeb3",
+    "url": "/Rainbow-2.0/static/css/main.1a21150d.chunk.css"
+  },
+  {
+    "revision": "35cac38a3e6741a2c6f6",
+    "url": "/Rainbow-2.0/static/js/2.abbe9e71.chunk.js"
+  },
+  {
+    "revision": "11a0481a58246bdcbeb3",
+    "url": "/Rainbow-2.0/static/js/main.d5921b36.chunk.js"
+  },
+  {
+    "revision": "31e2a414e6033184af59",
+    "url": "/Rainbow-2.0/static/js/runtime-main.ff2fc22e.js"
+  }
+]);
