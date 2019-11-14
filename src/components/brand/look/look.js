d9563ec1ec255af15f0157aaaa7d2c224973c30a
zaheer <zaheermalik284@gmail.com>
2019-11-14 19:51:11 +0500

    brand look color  get  completed

--- src/components/brand/look/look.js
+++ src/components/brand/look/look.js
@@ -2,29 +2,111 @@ import React, { Component } from 'react';
 import { GoLightBulb } from "react-icons/go";
 import './look.css'
 import Popup from '../../../shared/modal/modal';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Axios from 'axios';
 
 class look extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            show: false, 
+        this.state = {
+            show: false,
+            palette: null,
+            color0: '#fdffff',
+            color1: '#fdffff',
+            color2: '#fdffff',
+            color3: '#fdffff',
+            color4: '#fdffff',
+            percentage0: '',
+            percentage1: '',
+            percentage2: '',
+            percentage3: '',
+            percentage4: '',
+            CompanyBrandElementColorPaletteID: [],
+            url: 'http://ec2-34-198-96-172.compute-1.amazonaws.com//PatterService1/'
         }
     }
-    
+
+    componentDidMount = async () => {
+        try {
+
+            // get Color Palette
+            await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=5`).then(res => {
+                this.setState({ palette: res.data })
+
+                res.data.CompanyBrandElementColorPalette.forEach((v, i) => {
+                    let color = `color${i}`
+                    let percentage = `percentage${i}`
+                    let colVal = '#ffffff'
+                    if (v.ColorValue !== '') {
+                        colVal = v.ColorValue
+                    }
+                    this.setState({
+                        [color]: colVal,
+                        [percentage]: v.Percentage,
+                        CompanyBrandElementColorPaletteID: [...this.state.CompanyBrandElementColorPaletteID, v.CompanyBrandElementColorPaletteID]
+                    })
+                })
+            })
+        } catch (err) {
+            toast.error(err.message)
+        }
+    }
+
+
     handleClose = () => {
         this.setState({
-            show: false, 
+            show: false,
         })
     };
     handleShow = () => {
         this.setState({
-            show: true, 
+            show: true,
         })
     };
-    
+
+    changeColorHandler = (col, val) => {
+        this.setState({
+            [col]: val
+        })
+    }
+
+    changePercentageHandler = (col, old, val) => {
+        let sum = Number(this.state.percentage0) + Number(this.state.percentage1) + Number(this.state.percentage2) + Number(this.state.percentage3) + Number(this.state.percentage4);
+        let total = Number(sum) - Number(old) + Number(val);
+        console.log(Number(sum) + ' ' + Number(old) + ' ' + Number(val))
+        if (total <= 100) {
+            this.setState({
+                [col]: val
+            })
+        } else {
+            toast.warn('Percentage Limit Exceeded')
+        }
+    }
+
     render() {
-        const {show} = this.state;
+        const { show } = this.state;
         let style = { background: 'url(/images/brand-logo.png) no-repeat', backgroundSize: 'cover' }
+        let color_1 = {
+            backgroundColor: this.state.color0,
+            width: this.state.percentage0 + '%'
+        }
+        let color_2 = {
+            backgroundColor: this.state.color1,
+            width: this.state.percentage1 + '%'
+        }
+        let color_3 = {
+            backgroundColor: this.state.color2,
+            width: this.state.percentage2 + '%'
+        }
+        let color_4 = {
+            backgroundColor: this.state.color3,
+            width: this.state.percentage3 + '%'
+        }
+        let color_5 = {
+            backgroundColor: this.state.color4,
+            width: this.state.percentage4 + '%'
+        }
         return (
             <div className='look p-3'>
                 <div className='container'>
@@ -39,33 +121,33 @@ class look extends Component {
                         </div>
                         <label className='label mb-3'>Color Palette</label>
                         <div className='form-group flex'>
-                            <input type="text" className='form-control color' placeholder='Color #1' />
-                            <input type="text" className='form-control percentage' placeholder='%' />
+                            <input type="color" className='form-control color' value={this.state.color0} placeholder='Color #1' onChange={(e) => this.changeColorHandler('color0', e.target.value)} />
+                            <input type="number" className='form-control percentage' value={this.state.percentage0} placeholder='%' onChange={(e) => this.changePercentageHandler('percentage0', this.state.percentage0, e.target.value)} />
                             <span className='textarea_tooltip' onClick={this.handleShow} ><GoLightBulb /></span>
                         </div>
                         <div className='form-group flex'>
-                            <input type="text" className='form-control color' placeholder='Color #2' />
-                            <input type="text" className='form-control percentage' placeholder='%' />
+                            <input type="color" className='form-control color' value={this.state.color1} placeholder='Color #2' onChange={(e) => this.changeColorHandler('color1', e.target.value)} />
+                            <input type="number" className='form-control percentage' value={this.state.percentage1} placeholder='%' onChange={(e) => this.changePercentageHandler('percentage1', this.state.percentage1, e.target.value)} />
                         </div>
                         <div className='form-group flex'>
-                            <input type="text" className='form-control color' placeholder='Color #3' />
-                            <input type="text" className='form-control percentage' placeholder='%' />
+                            <input type="color" className='form-control color' value={this.state.color2} placeholder='Color #3' onChange={(e) => this.changeColorHandler('color2', e.target.value)} />
+                            <input type="number" className='form-control percentage' value={this.state.percentage2} placeholder='%' onChange={(e) => this.changePercentageHandler('percentage2', this.state.percentage2, e.target.value)} />
                         </div>
                         <div className='form-group flex'>
-                            <input type="text" className='form-control color' placeholder='Color #4' />
-                            <input type="text" className='form-control percentage' placeholder='%' />
+                            <input type="color" className='form-control color' value={this.state.color3} placeholder='Color #4' onChange={(e) => this.changeColorHandler('color3', e.target.value)} />
+                            <input type="number" className='form-control percentage' value={this.state.percentage3} placeholder='%' onChange={(e) => this.changePercentageHandler('percentage3', this.state.percentage3, e.target.value)} />
                         </div>
                         <div className='form-group flex'>
-                            <input type="text" className='form-control color' placeholder='Color #5' />
-                            <input type="text" className='form-control percentage' placeholder='%' />
+                            <input type="color" className='form-control color' value={this.state.color4} placeholder='Color #5' onChange={(e) => this.changeColorHandler('color4', e.target.value)} />
+                            <input type="number" className='form-control percentage' value={this.state.percentage4} placeholder='%' onChange={(e) => this.changePercentageHandler('percentage4', this.state.percentage4, e.target.value)} />
                         </div>
                         <div className='form-group flex mb-5'>
                             <div className='color_palette'>
-                                <span className='color_1'></span>
-                                <span className='color_2'></span>
-                                <span className='color_3'></span>
-                                <span className='color_4'></span>
-                                <span className='color_5'></span>
+                                <span className='color_1' style={color_1}></span>
+                                <span className='color_2' style={color_2}></span>
+                                <span className='color_3' style={color_3}></span>
+                                <span className='color_4' style={color_4}></span>
+                                <span className='color_5' style={color_5}></span>
                             </div>
                         </div>
                         <label className='label bold mb-3'>Style Assessment</label>
@@ -100,7 +182,7 @@ class look extends Component {
                         </div>
                     </form>
                 </div>
-                <Popup show={show} hide={this.handleClose}/>
+                <Popup show={show} hide={this.handleClose} />
             </div>
         );
     }
