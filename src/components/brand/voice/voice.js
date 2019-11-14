2669d6659b10d14d87c736869f6d4ae8b2cd0f0a
zaheer <zaheermalik284@gmail.com>
2019-11-14 19:40:41 +0500

    brand voice get  completed

--- src/components/brand/voice/voice.js
+++ src/components/brand/voice/voice.js
@@ -1,30 +1,92 @@
 import React, { Component } from 'react';
-import {  FaPlus } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
 import { GoLightBulb } from "react-icons/go";
 import './voice.css'
 import Popup from '../../../shared/modal/modal';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Axios from 'axios';
 
 class voice extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            show: false, 
+        this.state = {
+            show: false,
+            keywordInput: '',
+            buzzwordInput: '',
+            Keywords: null,
+            KeywordsValue: [],
+            Buzzwords: null,
+            BuzzwordsValue: [],
+            url: 'http://ec2-34-198-96-172.compute-1.amazonaws.com//PatterService1/'
         }
     }
-    
+
+    componentDidMount = async () => {
+        try {
+
+            // getKeyWords Data
+            await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=10`).then(res => {
+                console.log(res.data)
+                if (res.data.Value !== '') {
+                    this.setState({ KeywordsValue: res.data.Value.split(',') })
+                }
+                this.setState({ Keywords: res.data })
+            })
+
+            // getbuzzwords Data
+            await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=11`).then(res => {
+                console.log(res.data)
+                if (res.data.Value !== '') {
+                    this.setState({ BuzzwordsValue: res.data.Value.split(',') })
+                }
+                this.setState({ Buzzwords: res.data })
+            })
+
+        } catch (err) {
+            toast.error(err.message)
+        }
+    }
+
     handleClose = () => {
         this.setState({
-            show: false, 
+            show: false,
         })
     };
     handleShow = () => {
         this.setState({
-            show: true, 
+            show: true,
         })
     };
-    
+
+    addKeyword = () => {
+        this.setState({
+            KeywordsValue: [...this.state.KeywordsValue, this.state.keywordInput],
+            keywordInput: ''
+        })
+    }
+
+    addBuzzwords = () => {
+        this.setState({
+            BuzzwordsValue: [...this.state.BuzzwordsValue, this.state.buzzwordInput],
+            buzzwordInput: ''
+        })
+    }
+
+    removekeyword = (val) => {
+        this.setState({
+            KeywordsValue: this.state.KeywordsValue.filter(k => k != val)
+        })
+    }
+
+    removeBuzzword = (val) => {
+        this.setState({
+            BuzzwordsValue: this.state.BuzzwordsValue.filter(k => k != val)
+        })
+    }
+
     render() {
-        const {show} = this.state;
+        const { show } = this.state;
         return (
             <div className='brand voice p-3'>
                 <div className='container'>
@@ -33,34 +95,28 @@ class voice extends Component {
                         <h4 className='mb-4'>These are the brand elements in which the entire organization is built upon.</h4>
                         <div className='form-group mb-3'>
                             <label className='label'>Keywords</label>
-                            <input type="text" className='form-control' placeholder='Enter Keyword' />
-                            <FaPlus className='addPlus' />
+                            <input type="text" placeholder='Enter Keyword' className='form-control' value={this.state.keywordInput} onChange={(e) => this.setState({ keywordInput: e.target.value })} />
+                            <FaPlus className='addPlus pointer' onClick={this.addKeyword} />
                             <span className='textarea_tooltip' onClick={this.handleShow} ><GoLightBulb /></span>
                         </div>
                         <div className='form-group'>
                             <div className='tag_container'>
-                                <span className='note_msg'>Player</span>
-                                <span className='note_msg'>Gaming</span>
-                                <span className='note_msg'>Trips</span>
-                                <span className='note_msg'>Comp Management</span>
-                                <span className='note_msg'>Comp</span>
-                                <span className='note_msg'>Player Management</span>
+                                {
+                                    this.state.KeywordsValue.map((item, index) => <div key={index + item} className='note_msg'><span>{item}</span><span className='remove' onClick={() => this.removekeyword(item)} >✖</span></div>)
+                                }
                             </div>
                         </div>
                         <div className='form-group mb-3'>
                             <label className='label'>Buzzwords</label>
-                            <input type="text" className='form-control' placeholder='Enter Buzzword' />
-                            <FaPlus className='addPlus' />
+                            <input type="text" placeholder='Enter Buzzword' className='form-control' value={this.state.buzzwordInput} onChange={(e) => this.setState({ buzzwordInput: e.target.value })} />
+                            <FaPlus className='addPlus pointer' onClick={this.addBuzzwords} />
                             <span className='textarea_tooltip' onClick={this.handleShow} ><GoLightBulb /></span>
                         </div>
                         <div className='form-group'>
                             <div className='tag_container'>
-                                <span className='note_msg'>Player</span>
-                                <span className='note_msg'>Gaming</span>
-                                <span className='note_msg'>Trips</span>
-                                <span className='note_msg'>Comp Management</span>
-                                <span className='note_msg'>Comp</span>
-                                <span className='note_msg'>Player Management</span>
+                                {
+                                    this.state.BuzzwordsValue.map((item, index) => <div key={index + item} className='note_msg'><span>{item}</span><span className='remove' onClick={() => this.removeBuzzword(item)} >✖</span></div>)
+                                }
                             </div>
                         </div>
                         <div className='mt-3 mb-5'>
@@ -69,7 +125,7 @@ class voice extends Component {
                         </div>
                     </form>
                 </div>
-                <Popup show={show} hide={this.handleClose}/>
+                <Popup show={show} hide={this.handleClose} />
             </div>
         );
     }
