ce2bb4d12c22bdff595d14b304fd976035176feb
zaheer <zaheermalik284@gmail.com>
2019-11-14 19:01:22 +0500

    brand foundation get data completed

--- src/components/brand/foundation/foundation.js
+++ src/components/brand/foundation/foundation.js
@@ -2,31 +2,117 @@ import React, { Component } from 'react';
 import { Select } from 'dropdown-select';
 import { GoLightBulb } from "react-icons/go";
 import Popup from '../../../shared/modal/modal';
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Axios from 'axios';
 
 class foundation extends Component {
 
   constructor(props) {
     super(props);
-    this.state={
-        show: false, 
+    this.state = {
+      show: false,
+      options: [],
+      val1: '',
+      val2: '',
+      val3: '',
+      val4: '',
+      val5: '',
+
+      organizationalData:null,
+      CompanyOrganizationalValueIDs:[],
+
+      elevatorData: null,
+      elevatorDescDepartmentID: 0,
+      elevatorDesc: '',
+
+      missionData: null,
+      missionDescDepartmentID: 0,
+      missionDesc: '',
+
+      originData: null,
+      originDescDepartmentID: 0,
+      originDesc: '',
+
+      url: 'http://ec2-34-198-96-172.compute-1.amazonaws.com//PatterService1/'
     }
-}
+  }
 
-handleClose = () => {
-    this.setState({
-        show: false, 
-    })
-};
-handleShow = () => {
-    this.setState({
-        show: true, 
-    })
-};
+  componentDidMount = async () => {
+    try {
+      // Elevator
+      await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=1`).then(res => {
+        console.log(res)
+        this.setState({ elevatorData: res.data, elevatorDesc: res.data.Value })
+        if (res.data.Department != undefined) {
+          this.setState({ elevatorDescDepartmentID: res.data.Department.DepartmentID })
+        }
+      })
 
-render() {
-    const {show} = this.state;
-    let options = [{ label: 'label1', value: 'value1' }, { label: 'label2', value: 'value2' }]
+      // Origin
+      await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=2`).then(res => {
+        console.log(res)
+        this.setState({ originData: res.data, originDesc: res.data.Value })
+        if (res.data.Department != undefined) {
+          this.setState({ originDescDepartmentID: res.data.Department.DepartmentID })
+        }
+      })
+
+      // Mission
+      await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=4`).then(res => {
+        console.log(res)
+        this.setState({ missionData: res.data, missionDesc: res.data.Value })
+        if (res.data.Department != undefined) {
+          this.setState({ missionDescDepartmentID: res.data.Department.DepartmentID })
+        }
+      })
+
+      // getValues
+      await Axios.get(this.state.url + `getValues`).then(res => {
+        console.log(res)
+        this.setState({
+          options: res.data,
+        })
+      })
+
+      // Organizational
+      await Axios.get(this.state.url + `getCompanyBrandElement?companyID=${JSON.parse(localStorage.user).Company.CompanyID}&BrandElementID=3`).then(res => {
+        console.log(res)
+        res.data.CompanyBrandElementValues.forEach((v, i) => {
+            let vars = `val${i + 1}`
+            this.setState({ [vars]: v.Value,CompanyOrganizationalValueIDs:[...this.state.CompanyOrganizationalValueIDs,v.CompanyBrandElementValuesID] })
+        })
+        this.setState({
+            organizationalData: res.data,
+        })
+    })
+
+
+
+    } catch (err) {
+      toast.error(err.message)
+    }
+  }
+
+  handleClose = () => {
+    this.setState({
+      show: false,
+    })
+  };
+  handleShow = () => {
+    this.setState({
+      show: true,
+    })
+  };
+
+  changeHandler = (stVar, val) => {
+    this.setState({
+      [stVar]: val
+    })
+  }
+
+  render() {
+    const { show } = this.state;
     return (
       <div className='p-3'>
         <div className='container'>
@@ -35,41 +121,35 @@ render() {
           <form className='form'>
             <div className='form-group'>
               <label className='label'>Mission/ Purpose</label>
-              <textarea className='form-control textarea'>
-                Lorem ipsum dolor sit amet, duo te dicit impedit, pro mutat inermis delicata eu, sit ea esse aliquam suscipiantur. Ut vix libris invidunt.
-              </textarea>
+              <textarea className='form-control textarea' value={this.state.missionDesc} onChange={($event) => this.setState({ missionDesc: $event.target.value })} ></textarea>
               <span className='textarea_tooltip mt-4 pt-2' onClick={this.handleShow} ><GoLightBulb /></span>
             </div>
             <div className='form-group'>
               <label className='label'>Origin Story</label>
-              <textarea className='form-control textarea'>
-                Lorem ipsum dolor sit amet, duo te dicit impedit, pro mutat inermis delicata eu, sit ea esse aliquam suscipiantur. Ut vix libris invidunt.
-              </textarea>
+              <textarea className='form-control textarea' value={this.state.originDesc} onChange={($event) => this.setState({ originDesc: $event.target.value })} ></textarea>
               <span className='textarea_tooltip mt-4 pt-2' onClick={this.handleShow} ><GoLightBulb /></span>
             </div>
             <div className='form-group'>
               <label className='label'>Elevator Pitch</label>
-              <textarea className='form-control textarea'>
-                Lorem ipsum dolor sit amet, duo te dicit impedit, pro mutat inermis delicata eu, sit ea esse aliquam suscipiantur. Ut vix libris invidunt.
-              </textarea>
+              <textarea className='form-control textarea' value={this.state.elevatorDesc} onChange={($event) => this.setState({ elevatorDesc: $event.target.value })} ></textarea>
               <span className='textarea_tooltip mt-4 pt-2' onClick={this.handleShow} ><GoLightBulb /></span>
             </div>
             <div className='form-group'>
               <label className='label'>Organizational Values</label>
-              <Select placeholder='Humility' options={options} labelKey="label" valueKey="value" />
+              <Select placeholder='Humility' labelKey="ValuesName" valueKey="ValuesID" options={this.state.options} value={this.state.val1} onChange={(val) => this.changeHandler('val1', val)} />
               <span className='textarea_tooltip mt-4 pt-2' onClick={this.handleShow} ><GoLightBulb /></span>
             </div>
             <div className='form-group'>
-              <Select placeholder='Empathy' options={options} labelKey="label" valueKey="value" />
+              <Select placeholder='Empathy' labelKey="ValuesName" valueKey="ValuesID" options={this.state.options} value={this.state.val2} onChange={(val) => this.changeHandler('val2', val)} />
             </div>
             <div className='form-group'>
-              <Select placeholder='Collaboration' options={options} labelKey="label" valueKey="value" />
+              <Select placeholder='Collaboration' labelKey="ValuesName" valueKey="ValuesID" options={this.state.options} value={this.state.val3} onChange={(val) => this.changeHandler('val3', val)} />
             </div>
             <div className='form-group'>
-              <Select placeholder='Persistence' options={options} labelKey="label" valueKey="value" />
+              <Select placeholder='Persistence' labelKey="ValuesName" valueKey="ValuesID" options={this.state.options} value={this.state.val4} onChange={(val) => this.changeHandler('val4', val)} />
             </div>
             <div className='form-group'>
-              <Select placeholder='Speed' options={options} labelKey="label" valueKey="value" />
+              <Select placeholder='Speed' labelKey="ValuesName" valueKey="ValuesID" options={this.state.options} value={this.state.val5} onChange={(val) => this.changeHandler('val5', val)} />
             </div>
             <div className='mt-3 mb-5'>
               <button className='btn_green'>Save</button>
@@ -77,7 +157,7 @@ render() {
             </div>
           </form>
         </div>
-        <Popup show={show} hide={this.handleClose}/>
+        <Popup show={show} hide={this.handleClose} />
       </div>
     );
   }
